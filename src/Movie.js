100d7adae60d14d68f30bef7df66ab4f509829c8
KIM Jongwan <68538676+JK-Kim4@users.noreply.github.com>
2021-01-19 17:52:24 +0900

    test commit 21.01.19 jw

--- src/Movie.js
+++ src/Movie.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-function Movie({id, year, title, summary, poster}){
-    return <h5>{title}</h5>;
+function Movie({ id, year, title, summary, poster }) {
+  return <h5>{title}</h5>;
 }
 
 Movie.propTypes = {
-    id : PropTypes.number.isRequired,
-    year : PropTypes.number.isRequired,
-    title : PropTypes.string.isRequired,
-    summary : PropTypes.string.isRequired,
-    poster : PropTypes.string.isRequired
-}
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+};
 
 export default Movie;
